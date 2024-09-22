e6f050063f25772f285a42345ada06b4a64d92a7
Dylan Barduzzi <dlbarduzzi@gmail.com>
2024-09-22 09:45:38 -0500

    Remove github actions tests to fail on purpose

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,6 @@
 import { Container } from "@/components/container"
 
 export default function Home() {
-  const name = "hello"
   return (
     <div className="bg-white">
       <Container className="py-8">
