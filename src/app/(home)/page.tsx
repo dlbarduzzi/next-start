20c879a7bab7fbeb6ac4f7b1a5485608da53b283
Dylan Barduzzi <dlbarduzzi@gmail.com>
2024-09-22 09:44:08 -0500

    Test github actions to fail on lint

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,7 @@
-import { Container } from "@/components/container";
+import { Container } from "@/components/container"
 
 export default function Home() {
+  const name = "hello"
   return (
     <div className="bg-white">
       <Container className="py-8">
